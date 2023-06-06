09b1291ced7878c7168cfdcc9377b2616787053d
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-06 10:29:12 -0500

    add basic framework (requirements/exports) to user models, need schema

---
+++ models/reaction.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+
+    }
+);
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
---
+++ models/thought.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
