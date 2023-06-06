989168ff8bfb10cb82c773c1cd8ed78a6357011c
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-06 11:22:17 -0500

    add basic thought schema

--- models/thought.js
+++ models/thought.js
@@ -1,11 +1,26 @@
-// const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-// // const thoughtSchema = new Schema(
-// //     {
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => moment(timestamp).format('llll'),
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+);
 
-// //     }
-// // );
+const Thought = model('thought', thoughtSchema);
 
-// // const Thought = model('thought', thoughtSchema);
-
-// module.exports = Thought;
+module.exports = Thought;
