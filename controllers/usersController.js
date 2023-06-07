25d0c1b979f2baaa63e659094005f058cf104dd1
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-06-07 15:47:11 -0500

    add populate thoughts and friends data to get single user route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,7 +12,11 @@ module.exports = {
 
     async getSingleUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params.userId });
+            const user = await User
+                .findOne({ _id: req.params.userId })
+                .populate({ path: 'thoughts', select: '-__v' })
+                .populate({ path: 'friends', select: '-__v' });
+
 
             if (!user) {
                 return res.status(404).json({ message: 'No user with that ID' });
